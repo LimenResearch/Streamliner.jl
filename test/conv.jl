ec04c37275b58b87ead7c0a46c6df3c8d6617f1b
Mattia Bergomi <mattiagbergomi@gmail.com>
2023-01-17 17:25:57 +0100

    fix tests

--- test/conv.jl
+++ test/conv.jl
@@ -3,17 +3,10 @@ using Flux
 
 @testset "Conv test" begin
     conv_arch = Architecture("../static/conv.toml")
-    @test size(conv_arch.layers) == (2,)
-    @test conv_arch.layers[1] isa Conv && conv_arch.layers[1] isa Conv
-    t = rand([10, 10, 3, 10])
+    @test size(conv_arch.layers) == (4,)
+    @test conv_arch.layers[1] isa Conv && conv_arch.layers[2] isa Conv
+    t = Float32.(rand(28, 28, 1, 10))
     conv = get_model(conv_arch)
-    @test size(conv(t)) == (10, 10, 3, 10)
+    @test size(conv(t)) == (10, 10)
 end
 
-@testset "Conv classifier test" begin
-    conv_class_arch = Architecture("../static/conv_classigier.toml")
-    @test size(conv_class_arch.layers) == (3,)
-    t = rand([10, 10, 3, 10])
-    conv_class = get_model(conv_class_arch)
-    @test size(conv_class(t)) == (10, 10)
-end
--- test/mlp.jl
+++ test/mlp.jl
@@ -4,8 +4,8 @@ using Flux
 @testset "MLP test" begin
     mlp_arch = Architecture("../static/mlp.toml")
     @test size(mlp_arch.layers) == (2,)
-    @test mlp_arch.layers[1] isa Dense && mlp_arch.layers[1] isa Dense
-    t = rand(10, 5)
+    @test mlp_arch.layers[1] isa Dense && mlp_arch.layers[2] isa Dense
+    t = Float32.(rand(50, 5))
     mlp = get_model(mlp_arch)
     @test size(mlp(t)) == (10, 5)
 end
